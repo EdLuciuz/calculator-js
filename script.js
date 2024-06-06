cd27fca5615a9247502532036477340193b1796c
EdLuciuz <100480885+EdLuciuz@users.noreply.github.com>
2024-06-06 18:14:18 +0700

    Added functionality to the buttons

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const question = document.querySelector(".question")
 const answer = document.querySelector(".answer")
 
-const numBtn = document.querySelector("#num")
+const numBtn = document.querySelectorAll("#num")
+const opeBtn = document.querySelectorAll('.ope')
+const CBtn = document.querySelector('#clear')
+let display = ''
 
-numBtn.addEventListener('click', () => {
-    let content = numBtn.textContent
-    console.log(content)
+function operate() {
+
+}
+
+numBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        let numcontent = btn.textContent
+        display += numcontent
+        question.textContent = display
+    })
+})
+
+opeBtn.forEach((btn2) => {
+    btn2.addEventListener('click', () => {
+        let opecontent = btn2.textContent
+        display += opecontent
+        question.textContent = display
+    })
+})
+
+CBtn.addEventListener('click', () => {
+    display = ''
+    question.textContent =display
 })
